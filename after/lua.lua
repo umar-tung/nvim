c175d184337576c961971e96ba262026d9f75abc
Umar Tung <umartung@gmail.com>
2026-01-06 22:00:21 -0800

    Add minifiles

--- after/lua.lua
+++ after/lua.lua
@@ -1,2 +1,1 @@
 -- Files within this directory will be run after. This file will run specifically for lua filetypes
-vim.opt_local.shiftwidth = 4
--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.wrap = false
 
